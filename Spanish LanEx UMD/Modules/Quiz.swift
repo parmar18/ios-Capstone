f638a5c1b187f916f7306b25b51a33caccbe4adf
parmar18 <119753984+parmar18@users.noreply.github.com>
2023-11-06 22:03:01 -0500

    Add files via upload

---
+++ Spanish LanEx UMD/Modules/Quiz.swift
@@ -0,0 +1,14 @@
+//
+//  Quiz.swift
+//  Spanish LanEx UMD
+//
+//  Created by Sid Parmar on 11/6/23.
+//
+
+import Foundation
+
+struct Quiz {
+    let question: String
+    let options: [String]
+    let correctAnswer: String
+}
---
+++ Spanish LanEx UMD/Views/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Spanish LanEx UMD
+//
+//  Created by Sid Parmar on 11/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+   
+    @IBAction func Vocab(_ sender: Any) {
+    }
+    
+    @IBAction func FlashCard(_ sender: Any) {
+    }
+    
+    @IBAction func Quiz(_ sender: Any) {
+    }
+    // MARK: - Actions
+    @IBAction func didTapVocabularyButton(_ sender: UIButton) {
+           // Navigate to Daily Vocabulary section
+       }
+       
+    @IBAction func didTapFlashcardsButton(_ sender: UIButton) {
+           // Navigate to Flashcards section
+       }
+       
+    @IBAction func didTapQuizzesButton(_ sender: UIButton) {
+           // Navigate to Quizzes section
+       }
+       
+
+}
+
